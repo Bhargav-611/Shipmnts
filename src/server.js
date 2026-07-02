78c20b3fe50e58e17b119f1b6a51ad4afffdb5ab
Bhargav-611 <bhargav.chalodiya611@gmail.com>
2026-07-02 20:34:16 +0530

    Initial Boilerplate Commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`server running on port localhost:${PORT}`);
+    });
+})();
